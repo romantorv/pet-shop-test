36255cf645e14c874b30beced9f03ce51084bfe0
Vinh <quangvinhbach@gmail.com>
2019-07-02 09:30:34 +0800

    Updates

---
+++ precache-manifest.8799d0e154fe9080ba1d9acbeca1f183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad756168e14b31ad87be7665c5741b85",
+    "url": "/pet-shop-test/index.html"
+  },
+  {
+    "revision": "4fe1f402fe05f1d99f1d",
+    "url": "/pet-shop-test/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "54f7f6f4662171983393",
+    "url": "/pet-shop-test/static/css/main.c57d6e2e.chunk.css"
+  },
+  {
+    "revision": "4fe1f402fe05f1d99f1d",
+    "url": "/pet-shop-test/static/js/2.0d6515ee.chunk.js"
+  },
+  {
+    "revision": "54f7f6f4662171983393",
+    "url": "/pet-shop-test/static/js/main.c64b0d6a.chunk.js"
+  },
+  {
+    "revision": "2069ef071fc044c2f8b4",
+    "url": "/pet-shop-test/static/js/runtime~main.1db2aaa7.js"
+  }
+]);
