3191b0ef214a9075febeaa0fc26a57dd78548405
Vinh <quangvinhbach@gmail.com>
2019-07-02 10:01:40 +0800

    Update configuration for GithubPage

--- src/app.config.js
+++ src/app.config.js
@@ -3,4 +3,4 @@ export const REST_OWNERS = REST_BASE + '/owners';
 
 export const ROUTE_HOMEPAGE = '/';
 
-export const MEDIA_BASE = '/static/images';
+export const MEDIA_BASE = process.env.REACT_APP_MEDIA_BASE + '/static/images';
